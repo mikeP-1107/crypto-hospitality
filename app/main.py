956e79778974d12a890b0a0b464414f6ae744d4f
Mike P <Mpoblacion92@gmail.com>
2021-03-11 23:48:25 -0500

    Added user router to main file

--- app/main.py
+++ app/main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from .routers import business, crypto, users
 
 app = FastAPI()
 
+#app.include_router(business.router)
+#app.include_router(crypto.router)
+app.include_router(users.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Connecting crypto friendly businesses with crypto fanatic people"}
